5b7f39147d6509b548deaa1c6d6e827f2c717f71
lucasc <ciriacoborba077@gmail.com>
2025-01-15 21:48:15 -0300

    Adicionado o sistema para calcular a media

--- Ex007/Program.cs
+++ Ex007/Program.cs
@@ -4,8 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Qual sua nota total: ");
-            double media = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Sua primeira nota: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Sua segunda nota: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double num3 = num + num2;
+            double media = num3 / 2;
+            Console.WriteLine("A sua média é, {0}.", media);
+            
             if (media >= 6)
             {
                 Console.WriteLine("Aluno aprovado!");
