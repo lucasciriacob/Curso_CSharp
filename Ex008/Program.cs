c994074e32b057b64a2a413fb63e5f20ff6e6a2f
lucasc <ciriacoborba077@gmail.com>
2025-01-15 22:20:09 -0300

    Novos exs

---
+++ Ex008/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a velocidade: ");
+            double velocidade = Convert.ToDouble(Console.ReadLine());
+
+            if (velocidade > 80)
+            {
+                Console.WriteLine("Você foi multado! O limite é de 80KM/h");
+                //Quanto passou do limite. Multa custa R$ 7,00 por KM acima do limite
+                double multa = (velocidade - 80) * 7;
+                Console.Write("O valor da multa é de: R${0} ", multa);
+            }
+
+            else
+            {
+                Console.Write("Parabéns, dirigiu igual gente :) .");
+            }
+        }
+    }
+}
---
+++ Ex009/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("O número {0} é par.", num);
+            }
+
+            else
+            {
+                Console.WriteLine("O número {0} é ímpar.", num);
+
+            }
+        }
+    }
+}
