1b883fd81ed0caefde62caf37e9ee06002a4dc63
lucasc <ciriacoborba077@gmail.com>
2025-01-20 19:40:16 -0300

    NOVOS EXS

---
+++ Ex010/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Ex010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a distância em quilômetros: ");
+            double distancia = double.Parse(Console.ReadLine());
+
+            double tarifa = CalcularTarifa(distancia);
+
+            Console.WriteLine($"Tarifa: R$ {tarifa:F2}");
+            static double CalcularTarifa(double distancia)
+            {
+
+                if (distancia <= 200)
+                {
+                    return distancia * 0.50;
+
+                }
+                else
+                {
+                    return distancia *0.45;
+                }
+
+            }
+        }
+    }
+}
---
+++ Ex011/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("Informe o tamanho da primeira reta: "); 
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe o tamanho da segunda reta: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe o tamanho da terceira reta: ");
+            c = Convert.ToInt32(Console.ReadLine());    
+
+            
+            
+
+        }
+    }
+}
