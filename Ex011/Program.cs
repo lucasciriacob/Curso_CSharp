87cea62001ee89b81788a1c8ab5d132c5700cc49
lucasc <ciriacoborba077@gmail.com>
2025-01-20 20:04:51 -0300

    atualização ex11

--- Ex011/Program.cs
+++ Ex011/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design;
 
 namespace Ex011
 {
@@ -13,10 +14,25 @@ namespace Ex011
             Console.Write("Informe o tamanho da segunda reta: ");
             b = Convert.ToInt32(Console.ReadLine());
             Console.Write("Informe o tamanho da terceira reta: ");
-            c = Convert.ToInt32(Console.ReadLine());    
+            c = Convert.ToInt32(Console.ReadLine());
 
+
+            if ((a < b + c) && (b < a + c) && (c < a + b))
+            {
+                Console.WriteLine("É possível formar um triângulo com essas retas.");
+            }
+            else 
+            {
+                Console.WriteLine("Não é possível formar um triângulo com essas retas.");
+            }
             
-            
+
+
+
+
+
+
+
 
         }
     }
