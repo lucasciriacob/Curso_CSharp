b64f581ca9bf3eebe9db7e9973bf8ffb20e02949
lucasc <ciriacoborba077@gmail.com>
2025-01-22 22:09:32 -0300

    Aula 22/01

---
+++ Ex017/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string genero;
+
+            Console.Write("Digite o seu gênero: ");
+            genero = Console.ReadLine();
+
+            while (genero != "M" && genero != "F")
+            {
+                Console.Write("Digite a inicial do seu gênero: ");
+
+                genero = Console.ReadLine().ToUpper();
+            }
+            Console.WriteLine("Bem-vindos e bem-vindas ao curso de C#!");
+        }
+    }
+}
