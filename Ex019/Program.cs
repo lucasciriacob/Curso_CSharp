d4f328e248de6889f55c5f7917236827dca6f70c
lucasc <ciriacoborba077@gmail.com>
2025-01-27 19:04:22 -0300

    Aula 27/01

---
+++ Ex019/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+
+            /* while (num < 5)
+             {
+                 Console.WriteLine("Curso C#");
+             }
+            */
+
+            do
+            {
+                Console.WriteLine("Curso C#");
+            } while (num < 5);
+        }
+    }
+}
---
+++ Teste_senha/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Teste_senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "123456";
+            string senhaDig;
+
+            do
+            {
+                Console.Write("Digite a senha: ");
+                senhaDig = Console.ReadLine();
+            } while (senhaDig != senha);
+        }
+    }
+}
