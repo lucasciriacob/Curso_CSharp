c86b182ad6e236b6865c71bab40bcde0921739e2
lucasc <ciriacoborba077@gmail.com>
2025-01-29 21:42:11 -0300

    29/01

---
+++ AreaTriangulo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AreaTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========================");
+            Console.WriteLine("Calcular a Área do Triângulo");
+            Console.WriteLine("==========================");
+            Console.WriteLine();
+
+            Console.Write("Digite o tamanho da base: ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Digite a altura: ");
+            int r2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            
+
+            area(r, r2);
+
+        }
+        static void area(int reta, int reta2)
+        {
+            int a = reta * reta2 / 2;
+            Console.WriteLine("A área do triângulo é de = {0}m2", a);
+        }
+    }
+}
---
+++ Impressao/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Impressao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            escreva("Lucas Ciriaco Borba");
+            Console.WriteLine();
+            escreva("Palmeiras");
+            Console.WriteLine();
+            escreva("Oi");
+        }
+        static void escreva(string texto)
+        {
+            char caracter = '=';
+            int tamanho = texto.Length + 4;
+            string linha = new string(caracter, tamanho);
+            string txt_centralizado = texto.PadLeft(2 + texto.Length).PadRight(tamanho);
+            Console.WriteLine(linha);
+            Console.WriteLine(txt_centralizado);
+            Console.WriteLine(linha);
+        }
+    }
+}
